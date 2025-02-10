ebcea3e6c180698b447454a335371116746911e8
DenialHu <156979811+DenialHu@users.noreply.github.com>
2025-02-10 14:32:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
-mkdir -p ~/.nltk_data
-python -m nltk.downloader punkt
+#!/bin/bash
+
+# Buat direktori nltk_data jika belum ada
+mkdir -p nltk_data
+
+# Unduh data NLTK ke direktori nltk_data
+python3 -m nltk.downloader -d ./nltk_data punkt
